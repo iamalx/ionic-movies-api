627f6e3ad84ab48b7bb502ef6f6080b23665555d
Alex <reyesgx@gmail.om>
2018-08-12 01:27:47 -0700

    finished search by category

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../../pages/home/home';
 import { Action } from '../../../node_modules/rxjs/scheduler/Action';
+import { UserProvider} from '../../providers/user/user';
 
 /**
  * Generated class for the SearchPage page.
@@ -16,20 +17,43 @@ import { Action } from '../../../node_modules/rxjs/scheduler/Action';
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public _user: UserProvider,
+              public navParams: NavParams) {
   }
 
-  categories: string[] = ['Action', 'Comedy', 'Drama', 'Family', 'Horror', 'Mistery', 'Romance', 'Thriller']
+  categories: string[] = [
+    {name:'Action',
+    id: "28" }, 
+    {name:'Comedy',
+    id: "35" }, 
+    {name:'Drama',
+    id: "18" },
+    {name:'Family',
+    id: "10751" },
+    {name:'Horror',
+    id: "27" },
+    {name:'Mistery',
+    id: "9648" },
+    {name:'Romance',
+    id: "10749" }, 
+    {name:'Science Fiction',
+    id: "878" }, 
+    {name:'Thriller',
+    id: "53" }
+  ]
 
   itemSelected(item) {
+    this._user.isSearch = false;
     this.navCtrl.setRoot(HomePage, {item: item});
     console.log(item, "itemSlected()");
+    
   }
 
   i: any; 
   onSearch(value1) {
-    this.i = value1;
-    console.log(this.i, "getItems()");
+    console.log(value1, "onSearch()");
+    this._user.isSearch = true;
     this.navCtrl.setRoot(HomePage, {item: value1});
   }
 
--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { query } from '../../../node_modules/@angular/core/src/animation/dsl';
 
 /*
   Generated class for the UserProvider provider.
@@ -13,16 +14,26 @@ export class UserProvider {
   constructor(public http: HttpClient) {
     console.log('Hello UserProvider Provider');
   }
+  adressURL: string = 'https://api.themoviedb.org/3/'
 
-getData(movie) {
-  console.log('getData()1', movie);
-  if(movie !== undefined) {
-  console.log('user.getData()')
-  return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=0520d49060a9a8438b0fa54ee6d83bc0&language=en-US&query='+ movie + '&page=1&include_adult=false')
+isSearch: boolean;
+
+getData(query1) {
+  
+  if(query1 !== undefined) {
+    console.log( query1,'this.getData()first')
+    if(this.isSearch) {
+      console.log( query1,'this.getData()2')
+      return this.http.get(`${this.adressURL}search/movie?api_key=0520d49060a9a8438b0fa54ee6d83bc0&language=en-US&query=`+ query1 + '&page=1&include_adult=false')
+    }else {
+      console.log( query1,'this.getData()3')
+      return this.http.get(`${this.adressURL}discover/movie?api_key=0520d49060a9a8438b0fa54ee6d83bc0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${query1}`)
+    }  
   } else {
-  return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=0520d49060a9a8438b0fa54ee6d83bc0&language=en-US&query=action&page=1&include_adult=false')
+    console.log( query1,'this.getData()last')
+    return this.http.get(`${this.adressURL}movie/popular?api_key=0520d49060a9a8438b0fa54ee6d83bc0&language=en-US&page=1`)
   }
-}
+};
 
 
 }
